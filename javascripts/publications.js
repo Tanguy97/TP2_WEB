9cdce23e1e1630f0e1a687c8c15c8e3c754b7222
YuYu <jinyutung@gmail.com>
2019-10-08 16:52:08 -0400

    url

--- javascripts/publications.js
+++ javascripts/publications.js
@@ -2,6 +2,33 @@ function deletePublication(){
     $(this).parent().parent().remove()
 }
 
-
-
 $(".del-icon").click(deletePublication)
+
+function modifyUrl(){
+    const url=new URL(document.location.href)
+    const search_params=new URLSearchParams(url.search)
+    const parametre=search_params.get('sort_by')
+    const maValeur = (parametre == null) ? "date" : parametre
+    search_params.set('sort_by',$(this).val())
+    url.search=search_params
+    document.location.href=url
+}
+
+function actualiserPage(){
+    const url=new URL(document.location.href)
+    const search_params=new URLSearchParams(url.search)
+    const sortBy=search_params.get('sort_by')
+    const orderBy=search_params.get('order_by')
+    if(sortBy!=null){
+        $("#fieldFilterSection").val(sortBy)
+    }
+    else $("#fieldFilterSection").val('date')
+    if(orderBy!=null){
+        $("#filterAscValueSection").val(orderBy)
+    }
+    else $("#filterAscValueSection").val('desc')
+}
+
+actualiserPage();
+$("#fieldFilterSection").change(modifyUrl)
+
