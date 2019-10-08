4e3f322d415fe027ecef74e8f1226ee44dfc1895
YuYu <jinyutung@gmail.com>
2019-10-08 17:53:06 -0400

    layout

--- javascripts/layout.js
+++ javascripts/layout.js
@@ -1,22 +1,21 @@
 function header(){
     const url=new URL(document.location.href)
-    const currentPage = document.createElement('style')
 
     if (url.href==url.origin+"/"){
-        currentPage.innerHTML ="#home-link{background: var(--link-active-background-color); color: var(--nav-lang-background-color); border-radius: .25rem;}"
-        document.getElementById("home-link").appendChild(currentPage)
-    }
+        $('#home-link').addClass('link');
+        $('#home-link').addClass('active');
+   }
     else if(url==url.origin+"/team"){
-        currentPage.innerHTML ="#team-link{background: var(--link-active-background-color); color: var(--nav-lang-background-color); border-radius: .25rem;}"
-        document.getElementById("team-link").appendChild(currentPage)
+        $('#team-link').addClass('link');
+        $('#team-link').addClass('active');
     }
     else if(url==url.origin+"/projects"){
-        currentPage.innerHTML ="#projects-link{background: var(--link-active-background-color); color: var(--nav-lang-background-color); border-radius: .25rem;}"
-        document.getElementById("projects-link").appendChild(currentPage)
+        $('#projects-link').addClass('link');
+        $('#projects-link').addClass('active');
     }
     else if(url==url.origin+"/publications"){
-        currentPage.innerHTML ="#publications-link{background: var(--link-active-background-color); color: var(--nav-lang-background-color); border-radius: .25rem;}"
-        document.getElementById("publications-link").appendChild(currentPage)
+        $('#publications-link').addClass('link');
+        $('#publications-link').addClass('active');
     }
 }
 
