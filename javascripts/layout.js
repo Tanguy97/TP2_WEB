a441f9c61f7f07106394b6c90cfd35d9c731f759
Tanguy Gloaguen <tanguy.gloaguen@grenoble-inp.org>
2019-10-08 18:05:46 -0400

    modif nav

--- javascripts/layout.js
+++ javascripts/layout.js
@@ -6,15 +6,15 @@ function header(){
         currentPage.innerHTML ="#home-link{background: var(--link-active-background-color); color: var(--nav-lang-background-color); border-radius: .25rem;}"
         document.getElementById("home-link").appendChild(currentPage)
     }
-    else if(url==url.origin+"/team"){
+    else if((new RegExp("/team")).test(url.href)){
         currentPage.innerHTML ="#team-link{background: var(--link-active-background-color); color: var(--nav-lang-background-color); border-radius: .25rem;}"
         document.getElementById("team-link").appendChild(currentPage)
     }
-    else if(url==url.origin+"/projects"){
+    else if((new RegExp("/projects")).test(url.href)){
         currentPage.innerHTML ="#projects-link{background: var(--link-active-background-color); color: var(--nav-lang-background-color); border-radius: .25rem;}"
         document.getElementById("projects-link").appendChild(currentPage)
     }
-    else if(url==url.origin+"/publications"){
+    else if((new RegExp("/publications")).test(url.href)){
         currentPage.innerHTML ="#publications-link{background: var(--link-active-background-color); color: var(--nav-lang-background-color); border-radius: .25rem;}"
         document.getElementById("publications-link").appendChild(currentPage)
     }
