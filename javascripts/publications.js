4f193254e20e5424f1c3486d0f6075261b483f9e
Tanguy Gloaguen <tanguy.gloaguen@grenoble-inp.org>
2019-10-08 15:19:00 -0400

    commit

--- javascripts/publications.js
+++ javascripts/publications.js
@@ -2,6 +2,13 @@ function deletePublication(){
     $(this).parent().parent().remove()
 }
 
+function addAuthor(){
+    $("form > div.author-input").append("<input type='text' name='authors[]' placeholder='Auteurs' minlength='5' required=''>")
+    $("form > div.author-input").append("<i class='fa fa-minus fa-3x remove-author'></i>")
+}
 
-
+//Supression d'une publication
 $(".del-icon").click(deletePublication)
+
+//Ajout d'un auteur
+$(".add-author > i").click(addAuthor)
